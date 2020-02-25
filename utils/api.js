41151ea63841d79f094880a264662d7e8b6519ad
Ryan Kaplan <ryankaplan7@gmail.com>
2020-02-25 18:36:06 -0500

    added questions

--- utils/api.js
+++ utils/api.js
@@ -12,19 +12,5 @@ const api = {
   }
 };
 
-// ?access_token=${process.env.TOKEN}
-//?access_token=3527bf673a0c5af06df4c855c7517022d4f179d8
-
-//   getUser(user) {
-//     const queryUrl = `https://api.github.com/users/${user.username}/?per_page=100`;
-
-//     axios.get(queryUrl).catch(err => {
-//       console.log("error");
-//       process.exit(1);
-//     });
-//   }
-// }
-
-//api.getUser("space-age-pete")
 
 module.exports = api;
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,15 @@
 function generateMarkdown(answers, gitHubStuff) {
   return `
 # ${answers.title} \n
-Profile Pic: \n
 ![Profile Pic](${gitHubStuff.avatar_url}) \n
-Email: ${gitHubStuff.email} \n
-Description: ${ answers.description} \n
-Installation: ${ answers.installation} \n
+**Description:** *${ answers.description}* \n
+**Email:** ${gitHubStuff.email} \n
+**Installation:** ${ answers.installation} \n
+**Usage:** ${ answers.usage} \n
+**Licenses:** ${ answers.licenses} \n
+**Contributing:** ${ answers.contributing} \n
+**Testing:** ${ answers.testing} \n
+**Questions:** ${ answers.questions} \n
     `;
 }
 
